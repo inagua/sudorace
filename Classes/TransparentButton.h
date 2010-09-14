e828b464e42df91cf304b66a097bde0249c27826
Stephane Tavera <stephane@inagua.ch>
2010-09-14 19:40:24 +0200

    added transparent view to detect touch location

---
+++ Classes/TransparentButton.h
@@ -0,0 +1,16 @@
+//
+//  TransparentButton.h
+//  sudorace
+//
+//  Created by Stéphane Tavera on 9/14/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TransparentButton : UIView {
+
+}
+
+@end
---
+++ Classes/TransparentButton.m
@@ -0,0 +1,47 @@
+//
+//  TransparentButton.m
+//  sudorace
+//
+//  Created by Stéphane Tavera on 9/14/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "TransparentButton.h"
+
+
+@implementation TransparentButton
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if ((self = [super initWithFrame:frame])) {
+        // Initialization code
+    }
+    return self;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+	
+	UITouch *touch = [touches anyObject];	
+	float x = [touch locationInView:self].x;
+	float y = [touch locationInView:self].y;
+	
+	int i = (int)( x * 9 / self.bounds.size.width );
+	int j = (int)( y * 9 / self.bounds.size.height );
+	
+	NSLog(@"touch %d %d", i, j);	
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
